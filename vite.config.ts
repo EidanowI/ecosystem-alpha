9fab43d15c29da72c55c986cc83386c3e7413150
Fiodar Kavalchuk <141013183+EidanowI@users.noreply.github.com>
2025-11-14 18:00:06 +0300

    fix: deploy troubles

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/EcosystemAlpha/',
+  base: '/ecosystem-alpha/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
